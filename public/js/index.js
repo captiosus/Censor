53c511b027aa1a57bde61e52124df722d18ea04a
captiosus <royxu652@yahoo.com>
2016-02-20 17:45:59 -0600

    carousel

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,6 @@
+var SELECTOR_COLOR = "white";
+var SELECTOR_SELECTED = "black";
+
 var scan = document.getElementById("start-scan");
 var scan_line = document.getElementById("scan-line");
 
@@ -11,3 +14,26 @@ scan.addEventListener("mouseout", function(e) {
   scan_line.style.display = "none";
   scan_line.classList.remove("scan-line-anim");
 });
+
+var canvas = document.getElementById("index-drawings");
+var ctx = canvas.getContext("2d");
+
+ctx.rect(0, 0, 300, 300);
+ctx.fill();
+
+function selector_func() {
+  var number = (this.id).substr(9);
+  var selector = document.getElementsByClassName("selector-circle");
+  var i;
+  for (i = 0; i < selector.length; i++) {
+    selector[i].style.backgroundColor = SELECTOR_COLOR;
+  }
+  this.style.backgroundColor = SELECTOR_SELECTED;
+}
+
+var selector = document.getElementsByClassName("selector-circle");
+var i;
+for (i = 0; i < selector.length; i++) {
+  selector[i].addEventListener("click", selector_func);
+}
+
