b89b90f73683b3e8922e409fef9f5525c57b9e9a
captiosus <royxu652@yahoo.com>
2016-02-20 19:13:45 -0600

    basic anim

--- public/js/index.js
+++ public/js/index.js
@@ -18,8 +18,14 @@ scan.addEventListener("mouseout", function(e) {
 var canvas = document.getElementById("index-drawings");
 var ctx = canvas.getContext("2d");
 
-ctx.rect(0, 0, 300, 300);
-ctx.fill();
+ctx.fillStyle = "red";
+ctx.fillRect(0, 0, 300, 300);
+
+ctx.fillStyle = "black";
+ctx.fillRect(300, 0, 300, 300);
+
+ctx.fillStyle = "blue";
+ctx.fillRect(600, 0, 300, 300);
 
 function selector_func() {
   var number = (this.id).substr(9);
@@ -29,10 +35,29 @@ function selector_func() {
     selector[i].style.backgroundColor = SELECTOR_COLOR;
   }
   this.style.backgroundColor = SELECTOR_SELECTED;
+  var changed = 0;
+  var draw_interval = window.setInterval(function () {
+    if (changed < 300) {
+      changed+=5;
+      ctx.translate(-5, 0);
+      ctx.fillStyle = "red";
+      ctx.fillRect(0, 0, 300, 300);
+      
+      ctx.fillStyle = "black";
+      ctx.fillRect(300, 0, 300, 300);
+      
+      ctx.fillStyle = "blue";
+      ctx.fillRect(600, 0, 300, 300);
+    }
+    else {
+      clearInterval(draw_interval);
+    }
+  }, 5);
 }
 
 var selector = document.getElementsByClassName("selector-circle");
 var i;
+selector[0].style.backgroundColor = SELECTOR_SELECTED;
 for (i = 0; i < selector.length; i++) {
   selector[i].addEventListener("click", selector_func);
 }
