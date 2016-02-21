deadd1018e48413ccdb00a3e08bea7397eb5ac86
FelicityPictures <doodlefelicity@gmail.com>
2016-02-20 22:06:21 -0600

    document full

--- public/js/draw.js
+++ public/js/draw.js
@@ -9,7 +9,24 @@ c.beginPath();
 c.strokeStyle= "#000000";
 c.rect(130,5,340,440);
 c.stroke();
-c.font="20px Arial"
+
+c.font="15px Arial";
 c.textAlign="center";
-c.fillText("Classified Company",250,40);
+c.fillText("Classified Corp.",300,40);
+
+c.font="10px Arial";
+c.textAlign="start";
+c.fillText("John Smith",175,80);
+c.fillText("123 Bacon Avenue",175,90);
+c.fillText("New York, NY 10282",175,100);
+
+c.font="8px Arial";
+c.fillText("Dear Mr.Smith,",155,130);
+for(var i=0;i<24;i++){
+    c.fillText("blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah",165,143+10*i);
+}
+c.fillText("blah blah blah blah blah blah blah blah blah blah blah blah blah blah blah.",165,383);
+c.fillText("Sincerely,",335,400);
+c.font="10px Arial"
+c.fillText("The Staff of Classified",335,413);
 c.closePath();
