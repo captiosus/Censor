6bdda4e2ae5aa11d1a299d8cb09c35e20a642684
bleepbloopsify <bleepbloopsify@gmail.com>
2016-02-21 15:37:36 -0500

    Upload

--- app.js
+++ app.js
@@ -58,9 +58,10 @@ app.use(function(req, res, next){
 });
 app.post('/upload', function(req, res){
   console.log('hi');
-  console.log(req.files);
-  var file = req.files[0];
-  console.log(file.file);
+  var files = req.files;
+  for (var i = 0; i < files.length; i++){
+    var file = files[i];
+  }
   res.send(JSON.stringify(req.files));
 })
 
