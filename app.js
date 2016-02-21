6cb1014734878cdc51f2b4d825ee6748cef56294
bleepbloopsify <bleepbloopsify@gmail.com>
2016-02-20 19:21:38 -0500

    pls

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ var express = require('express');
 var app = express();
 var path = require('path');
 var tesseract = require('node-tesseract');
-swig = require('swig');
+var fs = require('fs');
+var utils = require("./utils")
 
+swig = require('swig');
 app.engine('html', swig.renderFile);
 
 app.set('view engine', 'html');
@@ -19,6 +21,22 @@ app.get('/', function (req, res) {
 
 app.get('/scan', function(req, res){
   res.render('scan');
+});
+
+app.post('/upload', function(req, res){
+  fs.readFile(req.files.image.path, function(err, data){
+    var imageName = req.files.image.name;
+    if (!imageName){
+      console.log("There was an error uploading this file");
+      res.redirect('/scan');
+      res.end();
+    }else{
+      var newPath = __dirname + "/uploads/uncensored/" + imageName;
+      fs.writeFile(newPath, data, function(err){
+        res.redirect("/view/" + imageName);
+      });
+    }
+  });
 });
 
 
