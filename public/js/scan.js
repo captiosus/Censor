366ff0e2a31da2f0fe96465669736013814346c0
captiosus <royxu652@yahoo.com>
2016-02-22 00:12:37 -0600

    multiple image fix

--- public/js/scan.js
+++ public/js/scan.js
@@ -4,6 +4,10 @@ var savebar = document.getElementById("savebar");
 var main = document.getElementById("main");
 var instructions = document.getElementById("instructions");
 var upload_more = document.getElementById("upload-more");
+var done = document.getElementById("done");
+var c = document.getElementById('censorme');
+var ctx = c.getContext("2d");
+var thumb_src;
 Dropzone.options.imagedropzone = {
   paramName:"image",
   maxFilesize: 2,
@@ -23,11 +27,12 @@ Dropzone.options.imagedropzone = {
             thumbnail.style.width="auto";
         }
         if (thumbnail.getAttribute('alt') == file.name){
-          console.log("match!");
-          thumbnail.parentNode.onclick =  function(){
-            console.log("processingfile", file.filename);
+          (((thumbnail.parentNode).parentNode).parentNode).onclick = function(){
+            c.style.display = "none";
+            ctx.clearRect(0,0,600, 450);
             main.style.display = "none";
             loading.style.display = "block";
+            thumb_src = this.getElementsByTagName("img")[0].getAttribute('src');
             thisdropzone.processFile(file);
           };
         }
@@ -37,11 +42,10 @@ Dropzone.options.imagedropzone = {
       drawBoxes(file, res);
       loading.style.display = "none";
       savebar.style.display = "block";
-      main.innerHTML = "<img src=\"" + thumbnail.getAttribute('src') + "\"/>";
     });
     this.on('addedfile', function(file) {
       if (!file.type.match(/image.*/)) {
-        myDropzone.emit("thumbnail", file, "http://path/to/image");
+        this.emit("thumbnail", file, "../image/pdf.png");
       }
       instructions.innerHTML = "CLICK ON IMAGE ON SIDEBAR OR UPLOAD MORE IMAGES";
     });
@@ -50,13 +54,16 @@ Dropzone.options.imagedropzone = {
     done();
   }
 }
+upload_more.addEventListener("click", function(e) {
+  e.preventDefault();
+  c.style.display = "none";
+  main.style.display ="block";
+})
 
 var drawBoxes = function(image, boxfile){
-  var c = document.getElementById('censorme');
   c.style.display = 'block';
-  var ctx = c.getContext("2d");
   var img = new Image();
-  img.src = thumbnail.getAttribute('src')
+  img.src = thumb_src;
   var aspect_ratio = image.height / image.width;
   var height = 600 * aspect_ratio;
   var ratio = height / image.height;
@@ -66,11 +73,9 @@ var drawBoxes = function(image, boxfile){
   for (var line in boxfile){
     line = boxfile[line].split(' ');//Char = index 0; xtopleft = index 1; ytopleft = index 2; xbottomright = index 3; ybottomright = index 4
     var box = [line[0]];
-    console.log(line);
     for (var i = 1; i < line.length - 1; i++){
       box.push( parseInt(line[i]) * ratio );
     }
-    console.log(box);
     ctx.beginPath();
     ctx.rect(box[1], height - box[2], Math.abs(box[3] - box[1]), -1 * Math.abs(box[4] - box[2]));
     ctx.stroke();
