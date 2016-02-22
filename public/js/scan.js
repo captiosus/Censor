435aee38c0e5fdf7d467812bcd6d6bc1b6899e51
FelicityPictures <doodlefelicity@gmail.com>
2016-02-21 22:23:11 -0600

    thumbnails resize BADLY now

--- public/js/scan.js
+++ public/js/scan.js
@@ -8,16 +8,23 @@ Dropzone.options.imagedropzone = {
   uploadMultiple: false,
   autoProcessQueue:false,
   init:function(){
-    var submitButton = document.querySelector("#upload")
+    var submitButton = document.querySelector("#upload");
     var thisdropzone = this;
 
     this.on('thumbnail', function(file, dataUrl){
       var thumbnails = document.getElementById('image-list').getElementsByTagName('img');
       for(var i = 0; i < thumbnails.length; i++){
         thumbnail = thumbnails[i];
+        if(thumbnail.getAttribute("width")>=thumbnail.getAttribute("height")){
+            thumbnail.style.width="160px";
+            thumbnail.style.height="auto";
+        }else{
+            thumbnail.style.height="160px";
+            thumbnail.style.width="auto";
+        }
         if (thumbnail.getAttribute('alt') == file.name){
           console.log("match!");
-          thumbnail.onclick =  function(){
+          thumbnail.onclick = function(){
             console.log("processingfile", file.filename);
             main.innerHTML = "";
             loading.style.display = "block";
@@ -25,7 +32,7 @@ Dropzone.options.imagedropzone = {
           };
         }
       }
-    })
+    });
     this.on('queuecomplete', function(){
       console.log("Queue completed");
     });
@@ -33,13 +40,13 @@ Dropzone.options.imagedropzone = {
       console.log(res);
       console.log("received response");
       loading.style.display = "none";
-      savebar.style.display = "block"
-      main.innerHTML = "<img src=\"" + thumbnail.getAttribute('src') + "\"/>"
-    })
+      savebar.style.display = "block";
+      main.innerHTML = "<img src=\"" + thumbnail.getAttribute('src') + "\"/>";
+    });
   },
   accept:function(file, done){
     console.log("new file!", file);
     done();
   },
 
-}
+};
