b8fee8cfe7878933ee2bda58cc6315493e624dd2
bleepbloopsify <bleepbloopsify@gmail.com>
2016-02-21 23:29:05 -0500

    pls

--- public/js/scan.js
+++ public/js/scan.js
@@ -1,32 +1,23 @@
-var main = document.getElementById("main");
-var loading = document.getElementById("loading");
-var savebar = document.getElementById("savebar");
-var thumbnail;
 Dropzone.options.imagedropzone = {
   paramName:"image",
   maxFilesize: 2,
   uploadMultiple: false,
   autoProcessQueue:false,
   init:function(){
-    var submitButton = document.querySelector("#upload")
+    // var submitButton = document.querySelector("#upload")
     var thisdropzone = this;
-<<<<<<< HEAD
-    submitButton.addEventListener("click", function() {
-      thisdropzone.processQueue();
-    });
-=======
->>>>>>> 851926c3007370214a8bb3894861dae33dd1a496
+    // submitButton.addEventListener("click", function() {
+    //   thisdropzone.processQueue();
+    // });
 
     this.on('thumbnail', function(file, dataUrl){
       var thumbnails = document.getElementById('image-list').getElementsByTagName('img');
       for(var i = 0; i < thumbnails.length; i++){
-        thumbnail = thumbnails[i];
+        var thumbnail = thumbnails[i];
         if (thumbnail.getAttribute('alt') == file.name){
           console.log("match!");
           thumbnail.onclick =  function(){
             console.log("processingfile", file.filename);
-            main.innerHTML = "";
-            loading.style.display = "block";
             thisdropzone.processFile(file);
           };
         }
@@ -38,14 +29,7 @@ Dropzone.options.imagedropzone = {
     this.on('success', function(file, res, err){
 
       console.log(res);
-<<<<<<< HEAD
       drawBoxes(file, res);
-=======
-      console.log("received response");
-      loading.style.display = "none";
-      savebar.style.display = "block"
-      main.innerHTML = "<img src=\"" + thumbnail.getAttribute('src') + "\"/>"
->>>>>>> 851926c3007370214a8bb3894861dae33dd1a496
     })
   },
   accept:function(file, done){
@@ -60,6 +44,7 @@ var drawBoxes = function(image, boxfile){
   c.style.visibility = 'visible';
   c.style.zIndex = 2;
   var ctx = c.getContext("2d");
+  // ctx.drawImage(, 0, 0);
   boxfile = boxfile.split('\n');
   console.log(boxfile);
   var boxes = []
